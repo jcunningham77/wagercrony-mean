6cd9d580880c04cb270744ea547b9fb371c50cac
Jeff Cunningham <jcunningham77@gmail.com>
2017-05-14 09:52:39 -0400

    troubleshooting

--- public/scripts/collapseNavController.js
+++ public/scripts/collapseNavController.js
@@ -1,4 +1,5 @@
 angular.module('wagerCrony').controller('collapseNavController', function ($scope) {
+  // debugger;
   $scope.isNavCollapsed = true;
   $scope.isCollapsed = false;
   $scope.isCollapsedHorizontal = false;
