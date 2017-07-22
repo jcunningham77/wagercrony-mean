41e33fae649c36685bde29e03e2ca771bbfd1ffb
Jeff Cunningham <jcunningham77@gmail.com>
2017-07-22 09:36:16 -0400

    remove commented lines

--- controller/teamDataController.js
+++ controller/teamDataController.js
@@ -12,42 +12,6 @@ module.exports = function(app){
 
         var teams= teamData.getTeamsByLeague(req.params.league);
 
-
-
-        // var teams = [
-        //           {"name":"Anaheim Ducks"},
-        //           {"name":"Arizona Coyotes"},
-        //           {"name":"Boston Bruins"},
-        //           {"name":"Buffalo Sabres"},
-        //           {"name":"Calgary Flames"},
-        //           {"name":"Carolina Hurricanes"},
-        //           {"name":"Chicago Blackhawks"},
-        //           {"name":"Colorado Avalanche"},
-        //           {"name":"Columbus Blue Jackets"},
-        //           {"name":"Dallas Stars"},
-        //           {"name":"Detroit Red Wings"},
-        //           {"name":"Edmonton Oilers"},
-        //           {"name":"Florida Panthers"},
-        //           {"name":"Los Angeles Kings"},
-        //           {"name":"Minnesota Wild"},
-        //           {"name":"Montreal Canadiens"},
-        //           {"name":"Nashville Predators"},
-        //           {"name":"New Jersey Devils"},
-        //           {"name":"New York Islanders"},
-        //           {"name":"New York Rangers"},
-        //           {"name":"Ottawa Senators"},
-        //           {"name":"Philadelphia Flyers"},
-        //           {"name":"Pittsburgh Penguins"},
-        //           {"name":"San Jose Sharks"},
-        //           {"name":"St. Louis Blues"},
-        //           {"name":"Tampa Bay Lightning"},
-        //           {"name":"Toronto Maple Leafs"},
-        //           {"name":"Vancouver Canucks"},
-        //           {"name":"Washington Capitals"},
-        //           {"name":"Winnipeg Jets"}
-        //         ]
-
-
                 res.status('200').send(teams);
                                    
     });
