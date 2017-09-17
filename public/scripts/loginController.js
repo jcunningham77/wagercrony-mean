ac397d56b305d53dbff9efb1fe9d7842acff4455
Jeff Cunningham <jcunningham77@gmail.com>
2017-09-17 09:53:17 -0400

    starting auth

--- public/scripts/loginController.js
+++ public/scripts/loginController.js
@@ -4,4 +4,12 @@ angular.module("wagerCrony")
 
   $scope.name = "Matt McMonigle";
   $scope.controllerName = "LoginController";
+
+  $scope.username = "";
+  $scope.password = "";
+
+  $scope.login = function(){
+    console.log('inside login function, username = ' + $scope.username + ', password = ' + $scope.password);
+
+  }
 });
