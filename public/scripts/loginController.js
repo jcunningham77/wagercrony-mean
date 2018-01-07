43c557ca0c886ee3525080872f9a6907bf0757e9
Jeff Cunningham <jcunningham77@gmail.com>
2018-01-07 16:51:32 -0500

    work with animations

--- public/scripts/loginController.js
+++ public/scripts/loginController.js
@@ -14,6 +14,20 @@ angular.module("wagerCrony")
     
   ];
 
+
+
+//   function sleep(ms) {
+//     return new Promise(resolve => setTimeout(resolve, ms));
+//   }
+  
+//   function demo(ms) {
+//     console.log('Taking a break...');
+//     await sleep(ms);
+//     console.log('Two second later');
+//   }
+  
+ 
+
   $scope.login = function(){
       dataService.login($scope.emailAddress,$scope.password).then(function(response) {
                 if (response.success) {
@@ -26,22 +40,45 @@ angular.module("wagerCrony")
                     console.log("pw:" + response.data.config.data.password);
                     authenticationService.SetCredentials(response.data);
                     // vm.dataLoading = false;
+                    // debugger;
+                    // var element = angular.element(document.getElementByName('submitButton'));
+                    var element = angular.element(document.getElementById('submitButton'));
 
-                    $location.path('/Track');
+                    
+                    console.log("loginController, login success, about to apply animation - time = " + new Date().toLocaleTimeString());
+                    // element.addClass('css-class'); 
+                    $animate.addClass(element,'move_up').then(function(){
+                        debugger;
+                        console.log('remove move_up');
+                        console.log("loginController, login success, about to remove animation - time = " + new Date().toLocaleTimeString());
+                        $animate.removeClass(element, 'move_up');
+                        $location.path('/Track');
+                    });
+                    // demo(2000);
+                    
+                    // setTimeout(function() {
+                    //     console.log("loginController, login success, after setTimeout= " + new Date().toLocaleTimeString());
+                    //     console.log("loginController, login success, about to navigate to track page = " + new Date().toLocaleTimeString());
+                        
+                    // }, (3 * 1000));
+                    
+
+                    
                 } else {
                     // debugger;
                     var element = angular.element(document).find(loginForm);
                     console.log('add shake class');
-                    $animate.addClass(element, 'shake', function() {
+                    console.log("loginController, login failure, about to apply shake animation - time = " + new Date().toLocaleTimeString());
+                    $animate.addClass(element, 'shake').then(function() {
+                        debugger;
+                        console.log("loginController, login failure, about to remove shake animation - time = " + new Date().toLocaleTimeString());
                         console.log('remove shake class');
                         $animate.removeClass(element, 'shake');
                       });
+
+                    console.log("loginController, login failure, about to pop alert time = " + new Date().toLocaleTimeString());  
                     $scope.alerts.push({type: 'danger',msg: response.message});
                     console.log("LoginController.login, response is failure, message from Backendless = " + response.message);
-                    //TODO work on flash service for error feedback
-              // flashService.Error(response.message);
-                    // vm.password="";
-                    // vm.dataLoading = false;
                 }
             });   
 
