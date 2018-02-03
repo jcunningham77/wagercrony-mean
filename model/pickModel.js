b6b34a1e1d6cd4c936cb4cb093c5396430676077
Jeff Cunningham <jcunningham77@gmail.com>
2018-02-02 21:45:31 -0500

    started picks refactor

--- model/pickModel.js
+++ model/pickModel.js
@@ -2,6 +2,10 @@ var mongoose = require ('mongoose');
 var Schema = mongoose.Schema;
 
 var pickSchema = new Schema({
+    pickTeam:String,
+    pickLine:String,
+    pineMoneyLine:String,
+    result:{ type: Number, min: -1, max: 1 },
     league:String,
     visitingTeam:String,
     homeTeam:String,
